de7250be19b2e27e402f81300ea56a4addb4e4b2
Andrea <andreascacchi10@gmail.com>
2023-11-11 15:54:01 +0100

    Player class added

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ and reports both Player's scores each round."""
 
 # these are the possible moves
 moves = ['rock', 'paper', 'scissors']
+
+
+"""The Player class is the parent class for all of the Players
+in this game"""
+
+class Player:
+    def move(self):
+        return 'rock'
+
+    def learn(self, my_move, their_move):
+        pass
