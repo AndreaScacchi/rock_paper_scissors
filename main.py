52485efe34e324eccb6a1ec3ffde7a1daca180a1
Andrea <andreascacchi10@gmail.com>
2023-11-11 15:51:41 +0100

    possible moves list added

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 """This program plays a game of Rock, Paper, Scissors between two Players,
 and reports both Player's scores each round."""
+
+
+# these are the possible moves
+moves = ['rock', 'paper', 'scissors']
