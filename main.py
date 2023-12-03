d2aac727334853b151e38a19919cc143ecd163cd
Andrea <andreascacchi10@gmail.com>
2023-12-03 16:44:49 +0100

    game finished

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ moves = ['rock', 'paper', 'scissors']
 """The Player class is the parent class for all of the Players
 in this game"""
 
+
 class Player:
     def move(self):
         return 'rock'
@@ -27,35 +28,42 @@ class RandomPlayer(Player):
         return random.choice(moves)
 
 
-# this subclass give you the possibility to play against the computer and make your moves
+# this subclass give you the possibility to play against the computer
+# and make your moves
 class HumanPlayer(Player):
     def move(self):
-        human_move = input('Make a move. Chosse between Rock, Paper or Scissors: ').lower()
-        if human_move in moves:
-            return human_move
-        print('This move is invalid, try again!')
+        while True:
+            human_move = input('Make a move.'
+                               'Choose Rock, Paper or Scissors: ').lower()
+            if human_move in moves:
+                return human_move
+            print('This move is invalid, try again!')
 
 
+# this subclass remembers the previous move the opponent played last round and
+# plays that move in the next round
 class ReflectPlayer(Player):
     def __init__(self):
         super().__init__()
         self.their_move = None
-    
+
     def move(self):
         if self.their_move is not None:
             return self.their_move
         else:
             return random.choice(moves)
-        
+
     def learn(self, my_move, their_move):
         self.their_move = their_move
 
 
+# this sublcass remembers what move it played last round and cycles through
+# the three moves
 class CyclePlayer(Player):
     def __init__(self):
         super().__init__()
         self.my_move = None
-    
+
     def move(self):
         if self.my_move is not None:
             index = moves.index(self.my_move)
@@ -63,17 +71,20 @@ class CyclePlayer(Player):
             return moves[next_move_index]
         else:
             return random.choice(moves)
-    
+
     def learn(self, my_move, their_move):
         self.my_move = my_move
 
 
+# this function tells whether one move beats another one
 def beats(one, two):
     return ((one == 'rock' and two == 'scissors') or
             (one == 'scissors' and two == 'paper') or
             (one == 'paper' and two == 'rock'))
 
 
+# this class starts the game. The play_round method simulates a single round
+# of the game. The play_game method executes the entire game.
 class Game:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -86,12 +97,12 @@ class Game:
         move2 = self.p2.move()
         print(f'Player 1: {move1} Player 2: {move2}')
         if beats(move1, move2):
-            print('Player 1 wins!')
+            print('Player 1 wins the round!')
             self.player1_score += 1
             print(f'Player 1 score = {self.player1_score}')
             print(f'Player 2 score = {self.player2_score}')
         elif beats(move2, move1):
-            print('Player 2 wins!')
+            print('Player 2 wins the round!')
             self.player2_score += 1
             print(f'Player 1 score = {self.player1_score}')
             print(f'Player 2 score = {self.player2_score}')
@@ -101,19 +112,29 @@ class Game:
         self.p2.learn(move2, move1)
 
     def play_game(self):
-        print("Game start!")
-        for round in range(3):  
-            print(f"Round {round}:")
+        print("GAME START!")
+        for round in range(3):
+            print(f"ROUND {round}:")
             self.play_round()
-        print(f"Final scores:")
-        print(f"Player 1: {game.player1_score}")
-        print(f"Player 2: {game.player2_score}")
-        print("Game over!")
+
+        if self.player1_score > self.player2_score:
+            print("Player 1 wins the game!")
+        elif self.player2_score > self.player1_score:
+            print("Player 2 wins the game!")
+        else:
+            print("It's a tie game!")
+
+        print(f"FINAL SCORES:")
+        print(f"PLAYER 1: {game.player1_score}")
+        print(f"PLAYER 2: {game.player2_score}")
+        print("GAME OVER!")
 
 
+# this code allows to test and observe how differnt player strategies
+# interact with a human player in the game.
 if __name__ == '__main__':
     game = Game(HumanPlayer(), ReflectPlayer())
     game.play_game()
-    
+
     cycle_game = Game(HumanPlayer(), CyclePlayer())
     cycle_game.play_game()
